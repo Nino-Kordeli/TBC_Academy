6b34d82685f21525bee78742f95f796d4dbff481
Nino-Kordeli <nino.kordeli.1@gmail.com>
2025-09-25 20:13:52 +0400

    polished

--- app/src/main/java/com/example/tbcacademy/Homework2.kt
+++ app/src/main/java/com/example/tbcacademy/Homework2.kt
@@ -32,7 +32,7 @@ fun main() {
 
         val result = when (operation) {
             "+" -> "The sum of $num1 and $num2 is ${num1 + num2}"
-            "-" -> "$num1 minus $num2 is ${num1 - num2} "
+            "-" -> "$num1 minus $num2 is ${num1 - num2}"
             "/" -> if (num2 == 0) "Can not divide by zero" else "$num1 divided by $num2 is ${num1.toDouble() / num2.toDouble()}"
             "*" -> "$num1 multiplied by $num2 is ${num1 * num2}"
             "%" -> "After division of $num1 and $num2 the remainder is ${num1 % num2}"
