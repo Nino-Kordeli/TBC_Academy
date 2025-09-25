3ac6464762064f03abd9e3d019d7677086ffb8fe
Nino-Kordeli <nino.kordeli.1@gmail.com>
2025-09-25 15:37:59 +0400

    initial commit

--- app/src/main/java/com/example/tbcacademy/Homework2.kt
+++ app/src/main/java/com/example/tbcacademy/Homework2.kt
@@ -1,47 +1,53 @@
 package com.example.tbcacademy
 
 fun main() {
-    println("Enter the first sequence: ")
-    val x = readln()
-    println("Enter the second sequence: ")
-    val y = readln()
-    println("Enter operation type: ")
-    val operation = readln()
+    var programStart = true
 
-
-    var numbersOnly1 = ""
-    var numbersOnly2 = ""
-    for (i in x) {
-        if (i.isDigit()) {
-            numbersOnly1 += i
+    while (programStart) {
+        println("Enter the first sequence: ")
+        val x = readln()
+        println("Enter the second sequence: ")
+        val y = readln()
+        println("Enter operation type: ")
+        val operation = readln()
+        var numbersOnly1 = ""
+        var numbersOnly2 = ""
+        for (i in x) {
+            if (i.isDigit()) {
+                numbersOnly1 += i
+            }
         }
-    }
-    println("First number: $numbersOnly1")
-    for (j in y) {
-        if (j.isDigit()) {
-            numbersOnly2 += j
+        println("First number: $numbersOnly1")
+        for (j in y) {
+            if (j.isDigit()) {
+                numbersOnly2 += j
+            }
         }
-    }
-    println("Second number: ${numbersOnly2}")
+        println("Second number: ${numbersOnly2}")
 
-    val num1 = numbersOnly1.toInt()
-    val num2 = numbersOnly2.toInt()
+        val num1 = numbersOnly1.toInt()
+        val num2 = numbersOnly2.toInt()
 
-    val result = when (operation) {
-        "+" -> "The sum of $num1 and $num2 is ${num1 + num2}"
-        "/" -> if (num2 == 0) "Can not divide by zero" else "$num1 divided by $num2 is ${num1 / num2}"
-        "*" -> "$num1 multiplied by $num2 is ${num1 * num2}"
-        "%" -> "After division of $num1 and $num2 the remainder is ${num1 % num2}"
-        "!" -> if (num2 == 0) {
-            "Can not divide by zero"
-        } else {
-            val division = num1 / num2
-            "Factorial of $num1 and $num2 division is ${factorial(division)}"
+        val result = when (operation) {
+            "+" -> "The sum of $num1 and $num2 is ${num1 + num2}"
+            "/" -> if (num2 == 0) "Can not divide by zero" else "$num1 divided by $num2 is ${num1 / num2}"
+            "*" -> "$num1 multiplied by $num2 is ${num1 * num2}"
+            "%" -> "After division of $num1 and $num2 the remainder is ${num1 % num2}"
+            "!" -> if (num2 == 0) {
+                "Can not divide by zero"
+            } else {
+                val division = num1 / num2
+                "Factorial of $num1 and $num2 division is ${factorial(division)}"
+            }
+
+            else -> "Invalid Operator"
         }
-
-        else -> "Invalid Operator"
+        println(result)
+        println("Do you want to restart the program <Y/N>?")
+        val userInput = readln()
+        programStart = userInput.equals("Y", ignoreCase = true)
     }
-    println(result)
+    println("Program Terminated")
 }
 
 fun factorial(n: Int): Int {
