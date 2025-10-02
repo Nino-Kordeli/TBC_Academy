2dcca401fff433ddab221410e3530a97003bce2b
Skooma421 <nino.kordeli@gmail.com>
2025-10-02 19:04:36 +0400

    Cleaned up code, added and improved helper and validation functions

--- app/src/main/java/com/example/tbcacademy/MainActivity.kt
+++ app/src/main/java/com/example/tbcacademy/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.tbcacademy
 
 import android.os.Bundle
+import android.util.Patterns
+import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,8 +13,7 @@ import com.example.tbcacademy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val users = mutableListOf<User>()
-    private var userCount = 0
+    private val users = mutableMapOf<String, User>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,31 +26,64 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        binding.addUserButton.setOnClickListener {
-            areFieldsEmpty()
-        }
-
-        binding.getUserInfoButton.setOnClickListener {
-
+        with(binding) {
+            addUserButton.setOnClickListener {
+                if (isEmailValid()) {
+                    inputValidation(
+                        name = nameField.getInputText(),
+                        email = emailField.getInputText()
+                    )
+                }
+            }
+            getUserInfoButton.setOnClickListener {
+                getUserInfo(
+                    email = emailField.getInputText()
+                )
+            }
         }
     }
 
-    private fun makeToast(text: String) {
-        Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
-    }
-
-    private fun areFieldsEmpty() {
-        val name = binding.nameField.text.toString().trim()
-        val email = binding.emailField.text.toString().trim()
+    private fun inputValidation(name: String, email: String) = with(binding) {
+        if (users.containsKey(email)) makeToast("This email is taken")
 
         if (name.isEmpty() || email.isEmpty()) {
             makeToast("Fill out all the fields")
         } else {
-            users.add(User(name, email))
-            userCount++
-            binding.userCount.text = "User Count -> $userCount"
-            binding.nameField.setText("")
-            binding.emailField.setText("")
+            users[email] = User(name, email)
+            userCountText.text = "Users -> ${users.size}"
+            nameField.text?.clear()
+            emailField.text?.clear()
         }
+    }
+
+    private fun getUserInfo(email: String) = with(binding) {
+        if (email.isEmpty()) {
+            makeToast("Please enter your email")
+            return
+        }
+        val user = users[email]
+        if (user == null) {
+            userNameContainer.text = ""
+            emailContainer.text = ""
+            userNotFoundText.visibility = View.VISIBLE
+        } else {
+            userNotFoundText.visibility = View.GONE
+            emailContainer.text = email
+            userNameContainer.text = user.name
+        }
+    }
+
+    private fun isEmailValid(): Boolean = with(binding) {
+        val email = emailField.text.toString()
+        return if ((!Patterns.EMAIL_ADDRESS.matcher(email).matches())) {
+            makeToast("Invalid Email")
+            false
+        } else true
+    }
+
+    private fun EditText.getInputText() = this.text.toString().trim()
+
+    private fun makeToast(text: String) {
+        Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
     }
 }
