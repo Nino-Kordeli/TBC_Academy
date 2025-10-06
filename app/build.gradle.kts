f6518395d9d50f7a094d56b4bfd33991e9f2cd6d
Nino-Kordeli <nino.kordeli.1@gmail.com>
2025-10-07 01:50:49 +0400

    finished design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.tbcacademy"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.tbcacademy"
-        minSdk = 36
-        targetSdk = 36
+        minSdk = 35
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
