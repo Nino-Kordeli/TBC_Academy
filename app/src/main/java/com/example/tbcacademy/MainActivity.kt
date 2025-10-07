ccf3df6911e008a741c2153e83c8949a0813ca5c
Nino-Kordeli <nino.kordeli.1@gmail.com>
2025-10-08 00:00:14 +0400

    polished

--- app/src/main/java/com/example/tbcacademy/MainActivity.kt
+++ app/src/main/java/com/example/tbcacademy/MainActivity.kt
@@ -28,35 +28,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun setText(textView: TextView, @StringRes stringRes: Int) {
-        textView.text = getString(stringRes)
-    }
-
-    private fun setImage(imageView: ImageView, @DrawableRes drawableRes: Int) {
-        imageView.setImageResource(drawableRes)
-    }
 
     private fun setResources() = with(binding) {
-        setImage(backgroundImage, R.drawable.background_image)
-        setImage(backButtonIcon, R.drawable.arrow_icon)
-        setImage(bookmarkButton, R.drawable.bookmark_icon)
-        setImage(locationIcon, R.drawable.location_pin_icon)
-        setImage(clockIcon, R.drawable.clock_icon)
-        setImage(weatherIcon, R.drawable.weather_icon)
-        setImage(starIcon, R.drawable.star_rating_icon)
-        setImage(bookButtonIcon, R.drawable.button_icon)
+        backgroundImage.setImageResource(R.drawable.background_image)
+        backButtonIcon.setImageResource(R.drawable.arrow_icon)
+        bookmarkButton.setImageResource(R.drawable.bookmark_icon)
+        locationIcon.setImageResource(R.drawable.location_pin_icon)
+        clockIcon.setImageResource(R.drawable.clock_icon)
+        weatherIcon.setImageResource(R.drawable.weather_icon)
+        starIcon.setImageResource(R.drawable.star_rating_icon)
+        bookButtonIcon.setImageResource(R.drawable.button_icon)
 
-        setText(titleText, R.string.andes_mountain)
-        setText(locationText, R.string.south_america)
-        setText(priceText, R.string.price)
-        setText(price, R.string._230)
-        setText(dollarSymbol, R.string.dollar_sign)
-        setText(overviewText, R.string.overview)
-        setText(detailsText, R.string.details)
-        setText(hoursText, R.string._8_hours)
-        setText(weatherText, R.string._16_c)
-        setText(starRatingText, R.string._4_5)
-        setText(descriptionText, R.string.large_text)
-        setText(bookNowButton, R.string.book_now)
+        titleText.setText(R.string.andes_mountain)
+        locationText.setText(R.string.south_america)
+        priceText.setText(R.string.price)
+        price.setText(R.string._230)
+        dollarSymbol.setText(R.string.dollar_sign)
+        overviewText.setText(R.string.overview)
+        detailsText.setText(R.string.details)
+        hoursText.setText(R.string._8_hours)
+        weatherText.setText(R.string._16_c)
+        starRatingText.setText(R.string._4_5)
+        descriptionText.setText(R.string.large_text)
+        bookNowButton.setText(R.string.book_now)
     }
 }
