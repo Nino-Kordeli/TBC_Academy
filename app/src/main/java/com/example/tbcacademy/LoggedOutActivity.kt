cb19104d4f31f165fbf3ac1f131eebcfbdae30b8
Nino-Kordeli <nino.kordeli.1@gmail.com>
2025-10-09 04:26:13 +0400

    changed titles

--- app/src/main/java/com/example/tbcacademy/LoggedOutActivity.kt
+++ app/src/main/java/com/example/tbcacademy/LoggedOutActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoggedOutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_logged_out)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
