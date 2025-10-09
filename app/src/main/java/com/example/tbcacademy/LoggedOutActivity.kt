b97d0e2d250a06c79f77eaadafba2b891ca618a4
Nino-Kordeli <nino.kordeli.1@gmail.com>
2025-10-09 20:11:33 +0400

    added button logic to logged out activity

--- app/src/main/java/com/example/tbcacademy/LoggedOutActivity.kt
+++ app/src/main/java/com/example/tbcacademy/LoggedOutActivity.kt
@@ -1,20 +1,37 @@
 package com.example.tbcacademy
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tbcacademy.databinding.ActivityLoggedOutBinding
 
 class LoggedOutActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoggedOutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_logged_out)
+        binding = ActivityLoggedOutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        setListeners()
+
+    }
+
+    private fun setListeners() = with(binding) {
+        btnRegister.setOnClickListener {
+            val intent = Intent(this@LoggedOutActivity, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+        btnLogIn.setOnClickListener {
+            val intent = Intent(this@LoggedOutActivity, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
