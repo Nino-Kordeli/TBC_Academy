a0018e8466e28f45a95e2c1d835819f5bf46c220
Nino-Kordeli <nino.kordeli.1@gmail.com>
2025-10-09 21:24:54 +0400

    added login logic

--- app/src/main/java/com/example/tbcacademy/LoginActivity.kt
+++ app/src/main/java/com/example/tbcacademy/LoginActivity.kt
@@ -1,20 +1,80 @@
 package com.example.tbcacademy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tbcacademy.databinding.ActivityLoginBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setListeners()
     }
+
+    private fun setListeners() = with(binding) {
+        btnNextLogin.setOnClickListener {
+            val email = etEmailFieldLogin.getString()
+            val password = etPasswordFieldLogin.getString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                showSnackbar(getString(R.string.please_fill_out_all_the_fields))
+                return@setOnClickListener
+            }
+            loginUser(email, password)
+        }
+    }
+
+    private fun loginUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this, RegistrationCompleteActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    intent.putExtra("email", email)
+                    startActivity(intent)
+                } else {
+                    val exception = task.exception
+                    when (exception) {
+                        is FirebaseAuthInvalidUserException -> {
+                            showSnackbar("User does not exist. Please register first.")
+                        }
+
+                        is FirebaseAuthInvalidCredentialsException -> {
+                            showSnackbar("Incorrect password.")
+                        }
+
+                        else -> {
+                            showSnackbar(exception?.localizedMessage ?: "Login failed")
+                        }
+                    }
+                }
+            }
+    }
+
+    private fun showSnackbar(message: String) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT).show()
+    }
+
+    private fun EditText.getString() = this.text.toString().trim()
 }
