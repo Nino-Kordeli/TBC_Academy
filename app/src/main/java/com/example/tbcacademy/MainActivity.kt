ea4ca2a364372a362ef788f649cb0766becdb0ec
Nino-Kordeli <nino.kordeli.1@gmail.com>
2025-10-12 15:39:35 +0400

    Added add user button functionality

--- app/src/main/java/com/example/tbcacademy/MainActivity.kt
+++ app/src/main/java/com/example/tbcacademy/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.tbcacademy
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.tbcacademy.databinding.ActivityMainBinding
+import com.example.tbcacademy.utils.showSnackBar
+import com.example.tbcacademy.utils.trimmedTextValue
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val userMap = mutableMapOf<String, User>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +23,49 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        setListeners()
+    }
+
+    private fun setListeners() = with(binding) {
+        btnAddUser.setOnClickListener {
+            val inputs = listOf(
+                etEmailField.trimmedTextValue(),
+                etNameField.trimmedTextValue(),
+                etLastnameField.trimmedTextValue(),
+                etAgeField.trimmedTextValue()
+            )
+            if (inputs.any { it.isEmpty() }) {
+                it.showSnackBar(getString(R.string.please_fill_out_all_the_fields))
+                return@setOnClickListener
+            }
+
+            val age = etAgeField.trimmedTextValue().toIntOrNull()
+            if (age == null) {
+                it.showSnackBar(getString(R.string.invalid_age))
+                return@setOnClickListener
+            }
+            val email = etEmailField.trimmedTextValue()
+            val firstName = etNameField.trimmedTextValue()
+            val lastName = etLastnameField.trimmedTextValue()
+
+            if (userMap.containsKey(email)) {
+                it.showSnackBar(getString(R.string.email_already_taken))
+                twResultMessage.text = getString(R.string.email_unavailable)
+                twResultMessage.setTextColor(Color.RED)
+                listOf(
+                    etNameField,
+                    etLastnameField,
+                    etEmailField,
+                    etAgeField
+                ).forEach { it.text?.clear() }
+
+            } else {
+                userMap[email] = User(firstName, lastName, age)
+                it.showSnackBar(getString(R.string.user_added_successfully))
+                twResultMessage.text = (getString(R.string.user_added))
+                twResultMessage.setTextColor(Color.GREEN)
+            }
         }
     }
 }
