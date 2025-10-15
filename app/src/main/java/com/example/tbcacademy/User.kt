629148e445e096713d0e300444a4af8b7b64dd99
Nino-Kordeli <nino.kordeli.1@gmail.com>
2025-10-15 08:55:23 +0400

    Added user data class

---
+++ app/src/main/java/com/example/tbcacademy/User.kt
@@ -0,0 +1,10 @@
+package com.example.tbcacademy
+
+data class User(
+    val id: Int,
+    val firstName: String,
+    val lastname: String,
+    val birthday: String,
+    val address: String,
+    val email: String
+)
