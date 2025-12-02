a2a2fe7fcc9e09f595bb9cb731480df8d37b234e
Nino-Kordeli <nino.kordeli.1@gmail.com>
2025-12-02 19:39:41 +0400

    Added viewmodel

---
+++ app/src/main/java/com/example/tbcacademy/presentation/lock_screen/vm/LockScreenViewModel.kt
@@ -0,0 +1,61 @@
+package com.example.tbcacademy.presentation.lock_screen.vm
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tbcacademy.domain.usecase.CheckPasscodeUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LockScreenViewModel @Inject constructor(
+    private val checkPasscodeUseCase: CheckPasscodeUseCase
+) : ViewModel() {
+
+    private val _input = mutableListOf<Int>()
+    val input: List<Int> get() = _input
+
+    private val _status = MutableStateFlow("")
+    val status: StateFlow<String> = _status
+
+    private val _dots = MutableStateFlow(listOf(false, false, false, false))
+    val dots: StateFlow<List<Boolean>> = _dots
+
+    fun addDigit(digit: Int) {
+        if (_input.size < 4) {
+            _input.add(digit)
+            updateDots()
+            if (input.size == 4) checkPassword()
+        }
+    }
+
+    fun removeDigit() {
+        if (_input.isNotEmpty()) {
+            _input.removeAt(_input.lastIndex)
+            updateDots()
+        }
+    }
+
+    fun updateDots() {
+        _dots.value = List(4) { index -> index < _input.size }
+    }
+
+    private fun checkPassword() {
+        val inputStr = _input.joinToString("")
+        viewModelScope.launch {
+            if (checkPasscodeUseCase.execute(inputStr)) {
+                _status.value = "success"
+            } else {
+                _status.value = "incorrect"
+            }
+            _input.clear()
+            updateDots()
+        }
+    }
+
+    fun resetStatus(){
+        _status.value = ""
+    }
+}
