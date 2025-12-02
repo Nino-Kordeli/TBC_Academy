158e0d161f08f99b1042af97e4fce3e038ae79e4
Nino-Kordeli <nino.kordeli.1@gmail.com>
2025-12-02 16:07:47 +0400

    Added

---
+++ app/src/main/java/com/example/tbcacademy/data/repository/RepositoryImpl.kt
@@ -0,0 +1,6 @@
+package com.example.tbcacademy.data.repository
+
+import javax.inject.Inject
+
+class RepositoryImpl @Inject constructor() {
+}
---
+++ app/src/main/java/com/example/tbcacademy/domain/repository/Repository.kt
@@ -0,0 +1,4 @@
+package com.example.tbcacademy.domain.repository
+
+class Repository {
+}
