eaf8150de3ca2121b71d52f85589ea431c755c42
Nino-Kordeli <nino.kordeli.1@gmail.com>
2026-01-15 02:26:34 +0400

    Modifications

--- app/src/main/java/com/example/tbcacademy/MainActivity.kt
+++ app/src/main/java/com/example/tbcacademy/MainActivity.kt
@@ -3,31 +3,11 @@ package com.example.tbcacademy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.tbcacademy.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
         }
     }
-}
-
-@Composable
-fun Greeting(message:String){
-    Text(text = message)
-}
-
-@Composable
-@Preview
-fun GreetingPreview(){
-    
 }
