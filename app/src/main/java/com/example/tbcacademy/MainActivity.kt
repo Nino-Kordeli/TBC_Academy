f820e6fe33c5eb09046538394f65b572f1acfad6
Nino-Kordeli <nino.kordeli.1@gmail.com>
2026-01-15 02:53:43 +0400

    Added welcome screen

--- app/src/main/java/com/example/tbcacademy/MainActivity.kt
+++ app/src/main/java/com/example/tbcacademy/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.tbcacademy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.tbcacademy.presentation.screen.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            WelcomeScreen()
         }
     }
 }
